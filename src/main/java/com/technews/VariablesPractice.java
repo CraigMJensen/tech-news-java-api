3ab5f1d95864bf16a2850d5365f4fdba5f667f15
CraigJensen <cmjensen82@gmail.com>
2022-05-01 16:30:06 -0500

    built structure for database

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+
+        int number = 35;
+        double price = 16.47;
+        String add = "Together";
+
+        double sum = number + price;
+        String added = add + number;
+
+        System.out.println(added);
+        System.out.println(sum);
+
+    }
+}
